8decd32a61337b33618f4450fa928e9e9c921d0c
Dmitrii Rog <dmitry.rog.jrn@gmail.com>
2023-06-30 19:01:29 +0200

    fix flag position

--- rootfs/usr/bin/run.sh
+++ rootfs/usr/bin/run.sh
@@ -9,4 +9,4 @@ else
     cheap_flag=""
 fi
 
-goofys -f ${ENDPOINT:+--endpoint $ENDPOINT} --region $REGION --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE --uid $UID --gid $GID -o $MOUNT_ACCESS $BUCKET $MOUNT_DIR "$cheap_flag"
+goofys -f ${ENDPOINT:+--endpoint $ENDPOINT} --region $REGION --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE --uid $UID --gid $GID -o $MOUNT_ACCESS "$cheap_flag" $BUCKET $MOUNT_DIR
