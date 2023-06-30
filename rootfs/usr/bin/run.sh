9beb2ea5ab605410644ab526661d34408719901b
Dmitrii Rog <dmitry.rog.jrn@gmail.com>
2023-06-30 18:43:39 +0200

    added cheap option

--- rootfs/usr/bin/run.sh
+++ rootfs/usr/bin/run.sh
@@ -3,5 +3,10 @@
 syslog-ng -f /etc/syslog-ng/syslog-ng.conf
 
 [[ "$UID" -ne "0" ]] && MOUNT_ACCESS="allow_other" || MOUNT_ACCESS="nonempty"
+if [[ -n $CHEAP ]]; then
+    cheap_flag="--cheap"
+else
+    cheap_flag=""
+fi
 
-goofys -f ${ENDPOINT:+--endpoint $ENDPOINT} --region $REGION --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE --uid $UID --gid $GID -o $MOUNT_ACCESS $BUCKET $MOUNT_DIR
+goofys -f ${ENDPOINT:+--endpoint $ENDPOINT} --region $REGION --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE --uid $UID --gid $GID -o $MOUNT_ACCESS $BUCKET $MOUNT_DIR "$cheap_flag"
