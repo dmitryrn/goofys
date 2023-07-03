534adf42255f446cbeff744679bafff8c8efe840
Dmitrii Rog <dmitry.rog.jrn@gmail.com>
2023-07-03 09:15:37 +0200

    remove unsupported mount opt

--- rootfs/usr/bin/run.sh
+++ rootfs/usr/bin/run.sh
@@ -2,7 +2,7 @@
 
 syslog-ng -f /etc/syslog-ng/syslog-ng.conf
 
-[[ "$UID" -ne "0" ]] && MOUNT_ACCESS="allow_other" || MOUNT_ACCESS="nonempty"
+[[ "$UID" -ne "0" ]] && MOUNT_OPTS="-o allow_other"
 if [[ -n $CHEAP ]]; then
     cheap_flag="--cheap"
 else
@@ -15,4 +15,4 @@ else
     no_implicit_dir_flag=""
 fi
 
-goofys -f ${ENDPOINT:+--endpoint $ENDPOINT} --region $REGION --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE --uid $UID --gid $GID -o $MOUNT_ACCESS "$cheap_flag" "$no_implicit_dir_flag" $BUCKET $MOUNT_DIR
+goofys -f ${ENDPOINT:+--endpoint $ENDPOINT} --region $REGION --stat-cache-ttl $STAT_CACHE_TTL --type-cache-ttl $TYPE_CACHE_TTL --dir-mode $DIR_MODE --file-mode $FILE_MODE --uid $UID --gid $GID $MOUNT_OPTS "$cheap_flag" "$no_implicit_dir_flag" $BUCKET $MOUNT_DIR
